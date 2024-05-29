1f51531d9a43e7a4803892016c42f9f6cc4abc2c
Jumadil-Muchtar <jumadilmuchtar431@gmail.com>
2024-05-30 07:39:41 +0800

    Menambahkan class Produk

---
+++ com/pos-system-oop-project/java/class/Produk.java
@@ -0,0 +1,33 @@
+public class Produk {
+    public String nama ;
+    private double harga;
+    private boolean statusPromo ;
+    private int diskon ;
+
+    public Produk(String namaParams, double hargaParams){
+        this.nama = namaParams;
+        this.harga = hargaParams;
+        this.statusPromo= false;
+        this.diskon = 0;
+    }
+    public void setPromo(boolean statusPromoParams, int diskon ){
+        this.statusPromo = statusPromoParams;
+        this.diskon = diskon;
+    }
+    public double getPromo() {
+        if (statusPromo){
+            return this.harga - (this.harga*this.diskon);
+        }else{
+            return 0.0;
+        }
+    }
+    public double getHarga() {
+        return this.harga;
+    }
+    public void setHarga(double hargaParams){
+        this.harga = hargaParams;
+    }
+
+
+
+}
